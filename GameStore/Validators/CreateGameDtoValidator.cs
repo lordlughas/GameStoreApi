e3fcb8d78573e4ec21bd85ab5e8057493ce907be
Charles Lughas <lordlughas@gmail.com>
2024-05-02 10:59:25 +0100

    Add validations

---
+++ GameStore/Validators/CreateGameDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using GameStore.Dtos;
+
+namespace GameStore.Validator;
+
+public class CreateGameDtoValidator : AbstractValidator<CreateGameDto>
+{
+    public CreateGameDtoValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Genre).NotEmpty();
+    }
+}
